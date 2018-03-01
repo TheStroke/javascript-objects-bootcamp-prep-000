80b14683c5375adecbf18e23c0d12b54e821d198
Aaron Gibson <aaron.gibson13@gmail.com>
2018-03-01 14:54:53 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -2,9 +2,9 @@ var playlist = {
   artist: "song title"
 }
 
-function updatePlaylist (object, artistName, songTitle) {
+function updatePlaylist (playlist, artistName, songTitle) {
   //add artist and song to playlist and return playlist
-  object.assign({}, object, artistName, songTitle)
-  return object
+  object.assign({}, playlist, artistName, songTitle)
+  return playlist
 }
 
